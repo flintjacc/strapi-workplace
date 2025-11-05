8a8cb6f49e637f558f522f7a4259253533b63b7a
flintjacc <flintjacc@gmail.com>
2025-11-06 03:33:27 +0800

    fix: allow Railway domain in admin

---
+++ config/admin.js
@@ -0,0 +1,20 @@
+﻿module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  url: '/admin',
+  serveAdminPanel: true,
+  allowedHosts: [
+    'strapi-workplace-production.up.railway.app',
+    'localhost',
+    '127.0.0.1'
+  ]
+});
