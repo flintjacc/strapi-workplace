d53a92636a2ba39ca37f1ce8c4ea0d294cb4fa64
flintjacc <flintjacc@gmail.com>
2025-11-06 13:43:20 +0800

    fresh start: clean package-lock.json and minimal config

---
+++ .history/config/admin_20251106124903.js
@@ -0,0 +1,29 @@
+﻿module.exports = ({ env }) => ({
+  auth: { secret: env('ADMIN_JWT_SECRET') },
+  apiToken: { salt: env('API_TOKEN_SALT') },
+  transfer: { token: { salt: env('TRANSFER_TOKEN_SALT') } },
+  url: '/admin',
+  serveAdminPanel: true,
+  // COMPLETELY DISABLE VITE SECURITY
+  webpack: (config, webpack) => {
+    config.devServer = {
+      ...config.devServer,
+      allowedHosts: 'all',
+      host: '0.0.0.0',
+      port: 1337,
+    };
+    return config;
+  },
+  // Force disable all security
+  config: {
+    admin: {
+      webpack: (config) => {
+        config.devServer = {
+          ...config.devServer,
+          allowedHosts: 'all',
+        };
+        return config;
+      },
+    },
+  },
+});
